4f8c9f015d7005d8e56355156ddf1a1fca9889e7
PriyanshuA2003 <priyansuagarkar2003@gmail.com>
2026-01-31 12:44:44 +0530

    Initial Commit

---
+++ backend/src/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { ENV } from "./lib/env.js";
+
+const app = express();
+
+app.get("/health", (req, res) => {
+    res.status(200).json({"msg" : "API is running"});
+})
+
+app.listen(3000, () => console.log("App running on port : ", ENV.PORT));
