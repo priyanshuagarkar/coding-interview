2f12cb3de116d2f95a2b93ed1c2fdf26634cb057
PriyanshuA2003 <priyansuagarkar2003@gmail.com>
2026-02-01 19:22:02 +0530

    Stream API configuration

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import User from "../models/User.js";
+import { deleteStreamUser, upsertStreamUser } from "./stream.js";
 
 export const inngest = new Inngest({
     id: "talent-iq"
@@ -23,6 +24,12 @@ const syncUser = inngest.createFunction(
         }
 
         await User.create(newUser);
+
+        await upsertStreamUser({
+            id: newUser.clerkId.toString(),
+            name: newUser.name,
+            image: newUser.profileImage
+        });
     }
 )
 
@@ -35,6 +42,8 @@ const deleteUserFromDB = inngest.createFunction(
         const {id} = event.data;
 
         await User.deleteOne({clerkId: id});
+
+        await deleteStreamUser(id.toString());
     }
 )
 
