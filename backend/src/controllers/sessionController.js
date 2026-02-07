8294a1d668f7484e2810adede49dcf4cd2eaed26
PriyanshuA2003 <priyansuagarkar2003@gmail.com>
2026-02-07 13:38:42 +0530

    session controller changes

--- backend/src/controllers/sessionController.js
+++ backend/src/controllers/sessionController.js
@@ -13,7 +13,7 @@ export async function createSession(req, res) {
         }
 
         // generate a unique call id for stream video
-        const callId = `session_${Date.now()}_{${Math.randon().toString(36).subString(7)}`;
+        const callId = `session_${Date.now()}_${Math.random().toString(36).substring(7)}`;
 
         // create session in db
         const session = await Session.create({ problem, difficulty, host: userId, callId });
@@ -100,10 +100,18 @@ export async function joinSession(req, res) {
 
         const session = await Session.findById(id);
 
-        if (!session) res.status(404).json({ message: "Session not found" });
+        if (!session) return res.status(404).json({ message: "Session not found" });
+
+        if(session.status !== "active") {
+            return res.status(400).json({ message: "Cannot join a completed session"});
+        }
+
+        if(session.host.toString() === userId.toString()) {
+            return res.status(400).json({ message: "Host cannot join their own session as participant"});
+        }
 
         // check if session is already full - has a participant
-        if (session.participant) return res.status(404).json(message: "Session is full");
+        if (session.participant) return res.status(409).json({message: "Session is full"});
 
         session.participant = userId;
         await session.save()
@@ -125,7 +133,7 @@ export async function endSession(req, res) {
 
         const session = await Session.findById(id);
 
-        if (!session) res.status(404).json({ message: "Session not found" });
+        if (!session) return res.status(404).json({ message: "Session not found" });
 
         // check if user is the host
         if (session.host.toString() !== userId.toString())
@@ -136,9 +144,6 @@ export async function endSession(req, res) {
             return res.status(400).json({ message: "Session is already completed" });
         }
 
-        session.status = "completed"
-        await session.save()
-
         // delete stream video call
         const call = streamClient.video.call("default", session.callId);
         await call.delete({hard:true});
@@ -146,6 +151,9 @@ export async function endSession(req, res) {
         // delete stream chat channel
         const channel = chatClient.channel("messaging", session.callId);
         await channel.delete();
+
+        session.status = "completed"
+        await session.save()
 
         res.status(200).json({ session, message: "Session ended successfully"});
     } catch (error) {
